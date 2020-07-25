d62c88b1d68f2bcfaf7335a9f14086193d164e9d
Austin Burton <abur3460@gmail.com>
2020-07-25 15:41:58 -0400

    migrate files

--- stock-market-scraper.py
+++ stock-market-scraper.py
@@ -1,4 +1,4 @@
-import urllib.request, json , time, os, difflib, itertools
+import urllib.request, json, time, os, difflib, itertools, shutil
 import pandas as pd
 from multiprocessing.dummy import Pool
 from datetime import datetime
@@ -8,10 +8,11 @@ try:
 except:
     import http.client as httplib
 
+
 def check_internet():
-    
+
     conn = httplib.HTTPConnection("www.google.com", timeout=5)
-    
+
     try:
         conn.request("HEAD", "/")
         conn.close()
@@ -22,14 +23,18 @@ def check_internet():
         # print("False")
         return False
 
-def get_historic_price(query_url,json_path,csv_path):
-    
-    stock_id=query_url.split("&period")[0].split("symbol=")[1]
 
-    if os.path.exists(csv_path+stock_id+'.csv') and os.stat(csv_path+stock_id+'.csv').st_size != 0:
-        print("<<<  Historical data of "+stock_id+" already exists")
+def get_historic_price(query_url, json_path, csv_path):
+
+    stock_id = query_url.split("&period")[0].split("symbol=")[1]
+
+    if (
+        os.path.exists(csv_path + stock_id + ".csv")
+        and os.stat(csv_path + stock_id + ".csv").st_size != 0
+    ):
+        print("<<<  Historical data of " + stock_id + " already exists")
         return
-    
+
     while not check_internet():
         print("Could not connect, trying again in 5 seconds...")
         time.sleep(5)
@@ -37,74 +42,111 @@ def get_historic_price(query_url,json_path,csv_path):
     try:
         with urllib.request.urlopen(query_url) as url:
             parsed = json.loads(url.read().decode())
-    
+
     except:
-        print("|||  Historical data of "+stock_id+" doesn't exist")
+        print("|||  Historical data of " + stock_id + " doesn't exist")
         return
-    
+
     else:
-        if os.path.exists(json_path+stock_id+'.json') and os.stat(json_path+stock_id+'.json').st_size != 0:
-            os.remove(json_path+stock_id+'.json')
-        
-        with open(json_path+stock_id+'.json', 'w') as outfile:
+        if (
+            os.path.exists(json_path + stock_id + ".json")
+            and os.stat(json_path + stock_id + ".json").st_size != 0
+        ):
+            os.remove(json_path + stock_id + ".json")
+
+        with open(json_path + stock_id + ".json", "w") as outfile:
             json.dump(parsed, outfile, indent=4)
-        
+
         try:
-            Date=[]
-            for i in parsed['chart']['result'][0]['timestamp']:
-                Date.append(datetime.utcfromtimestamp(int(i)).strftime('%d-%m-%Y'))
+            Date = []
+            for i in parsed["chart"]["result"][0]["timestamp"]:
+                Date.append(datetime.utcfromtimestamp(int(i)).strftime("%d-%m-%Y"))
 
-            Low=parsed['chart']['result'][0]['indicators']['quote'][0]['low']
-            Open=parsed['chart']['result'][0]['indicators']['quote'][0]['open']
-            Volume=parsed['chart']['result'][0]['indicators']['quote'][0]['volume']
-            High=parsed['chart']['result'][0]['indicators']['quote'][0]['high']
-            Close=parsed['chart']['result'][0]['indicators']['quote'][0]['close']
-            Adjusted_Close=parsed['chart']['result'][0]['indicators']['adjclose'][0]['adjclose']
+            Low = parsed["chart"]["result"][0]["indicators"]["quote"][0]["low"]
+            Open = parsed["chart"]["result"][0]["indicators"]["quote"][0]["open"]
+            Volume = parsed["chart"]["result"][0]["indicators"]["quote"][0]["volume"]
+            High = parsed["chart"]["result"][0]["indicators"]["quote"][0]["high"]
+            Close = parsed["chart"]["result"][0]["indicators"]["quote"][0]["close"]
+            Adjusted_Close = parsed["chart"]["result"][0]["indicators"]["adjclose"][0][
+                "adjclose"
+            ]
 
-            df=pd.DataFrame(list(zip(Date,Low,Open,Volume,High,Close,Adjusted_Close)),columns =['Date','Low','Open','Volume','High','Close','Adjusted Close'])
+            df = pd.DataFrame(
+                list(zip(Date, Low, Open, Volume, High, Close, Adjusted_Close)),
+                columns=[
+                    "Date",
+                    "Low",
+                    "Open",
+                    "Volume",
+                    "High",
+                    "Close",
+                    "Adjusted Close",
+                ],
+            )
 
-            if os.path.exists(csv_path+stock_id+'.csv'):
-                os.remove(csv_path+stock_id+'.csv')
-            df.to_csv(csv_path+stock_id+'.csv', sep=',', index=None)
-            print(">>>  Historical data of "+stock_id+" saved")
-        
+            if os.path.exists(csv_path + stock_id + ".csv"):
+                os.remove(csv_path + stock_id + ".csv")
+            df.to_csv(csv_path + stock_id + ".csv", sep=",", index=None)
+            print(">>>  Historical data of " + stock_id + " saved")
+
         except:
-            print(">>>  Historical data of "+stock_id+" could not be saved")
-        
+            print(">>>  Historical data of " + stock_id + " could not be saved")
+
         return
+
 
 def main():
 
-	json_path = os.getcwd()+os.sep+".."+os.sep+"historic_data"+os.sep+"json"+os.sep
-	csv_path = os.getcwd()+os.sep+".."+os.sep+"historic_data"+os.sep+"csv"+os.sep
+    json_path = (
+        os.getcwd() + os.sep + os.sep + "historic_data" + os.sep + "json" + os.sep
+    )
+    csv_path = os.getcwd() + os.sep + os.sep + "historic_data" + os.sep + "csv" + os.sep
 
-	if not os.path.isdir(json_path):
-	    os.makedirs(json_path)
-	if not os.path.isdir(csv_path):
-	    os.makedirs(csv_path)
+    shutil.rmtree(
+        "C:/Users/OG/Documents/Code/1- Non-LAMBDA code/2-stock-market-scraper/historic_data/json"
+    )
+    print("Cleared all json data, ready to retrieve new")
+    shutil.rmtree(
+        "C:/Users/OG/Documents/Code/1- Non-LAMBDA code/2-stock-market-scraper/historic_data/csv"
+    )
+    print("Cleared all csv data, ready to retrieve new")
 
-	ticker_file_path = "Assets"+os.sep+"Yahoo Ticker Symbols - September 2017.xlsx"
+    if not os.path.isdir(json_path):
+        os.makedirs(json_path)
+    if not os.path.isdir(csv_path):
+        os.makedirs(csv_path)
 
-	temp_df = pd.read_excel(ticker_file_path)
-	temp_df = temp_df.drop(temp_df.columns[[5, 6, 7]], axis=1)
+    ticker_file_path = "Assets" + os.sep + "Yahoo Ticker Symbols - September 2017.xlsx"
 
-	headers = temp_df.iloc[2]
-	df  = pd.DataFrame(temp_df.values[3:], columns=headers)
-	print("Total stocks:",len(df))
+    temp_df = pd.read_excel(ticker_file_path)
+    temp_df = temp_df.drop(temp_df.columns[[5, 6, 7]], axis=1)
 
-	query_urls=[]
+    headers = temp_df.iloc[2]
+    df = pd.DataFrame(temp_df.values[3:], columns=headers)
+    print("Total stocks:", len(df))
 
-	for ticker in df['Ticker']:
-	    query_urls.append("https://query1.finance.yahoo.com/v8/finance/chart/"+ticker+"?symbol="+ticker+"&period1=0&period2=9999999999&interval=1d&includePrePost=true&events=div%2Csplit")
+    query_urls = []
 
-	with Pool(processes=10) as pool:
-	    pool.starmap(get_historic_price, zip(query_urls, itertools.repeat(json_path), itertools.repeat(csv_path)))
+    for ticker in df["Ticker"]:
+        query_urls.append(
+            "https://query1.finance.yahoo.com/v8/finance/chart/"
+            + ticker
+            + "?symbol="
+            + ticker
+            + "&period1=0&period2=9999999999&interval=1d&includePrePost=true&events=div%2Csplit"
+        )
 
-	print("<|>  Historical data of all stocks saved")
-	
-	return
+    with Pool(processes=10) as pool:
+        pool.starmap(
+            get_historic_price,
+            zip(query_urls, itertools.repeat(json_path), itertools.repeat(csv_path)),
+        )
+
+    print("<|>  Historical data of all stocks saved")
+
+    return
 
 
+if __name__ == "__main__":
+    main()
 
-if __name__ == '__main__':
-	main()
